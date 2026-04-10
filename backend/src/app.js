9e21629d8e6bb02df16250e15e115aa4224bf559
Debayan Saha <debayans526@gmail.com>
2026-04-10 21:52:46 +0530

    added cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.get('/',(req,res)=>{
 });
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://bite-craft.netlify.app"],
+    origin: ["http://localhost:5173"],
     credentials: true
 }));
 
