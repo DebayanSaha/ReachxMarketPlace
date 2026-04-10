ea3b1b1cfe827d0671037275a6b6d67d8ef5dfb9
Debayan Saha <debayans526@gmail.com>
2026-04-10 16:07:54 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const app = require('./src/app')
 require('dotenv').config();
 
+const app = require('./src/app')
+const connectDb = require('./src/db/db')
 const PORT = process.env.PORT;
 
+connectDb();
+
 app.listen(PORT,()=>{
-    console.log('Server is running...')
+    console.log(`Server is running on port- ${PORT}`)
 })
