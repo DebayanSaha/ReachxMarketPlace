efbef37622271b0145d5849cc9b6f0f479e1ae7a
Debayan Saha <debayans526@gmail.com>
2026-04-12 13:58:13 +0530

    Added new link

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,17 @@ import MartketPlace from './pages/MartketPlace'
 const App = () => {
   return (
     <div>
-      <img src="/logo/image.png" alt="Logo" className="ml-4 mt-4 top-6 left-6 h-12 rounded-full z-50 shadow-md" />
+      <a
+  href="https://reachxgroup.netlify.app/"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <img
+    src="/logo/image.png"
+    alt="Logo"
+    className="ml-4 mt-4 top-6 left-6 h-12 rounded-full z-50 shadow-md cursor-pointer"
+  />
+</a>
       <MartketPlace/>
     </div>
   )
