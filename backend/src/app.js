14a61b3b35eb2563f51cc9a7ab2f0b32f9f89a60
Debayan Saha <debayans526@gmail.com>
2026-04-14 11:35:43 +0530

    Added link

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,8 @@ app.use(cors({
     origin: [
         "http://localhost:5173",
         "https://reachx-admin.vercel.app",
-        "https://reachx-market-place.vercel.app"
+        "https://reachx-market-place.vercel.app",
+        "https://reachxgroup.netlify.app/",
     ],
     credentials: true
 }));
